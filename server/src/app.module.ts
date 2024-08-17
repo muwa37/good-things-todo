503239c424a1b9abe1c3ca71fab0983920b5759d
muwa37 <yond41m3@icloud.com>
2024-08-17 23:40:16 +0400

    Modified structure

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
+import { TodoModule } from './todo/todo.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env`,
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(`${process.env.MONGODB_CONNECTION_URL}`),
+    UserModule,
+    TodoModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}

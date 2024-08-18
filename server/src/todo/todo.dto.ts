02e2c55c8b2c062b84738c21ee6db8b3f5c04569
muwa37 <yond41m3@icloud.com>
2024-08-18 21:10:30 +0400

    Added validation pipe

--- server/src/todo/todo.dto.ts
+++ server/src/todo/todo.dto.ts
@@ -1,5 +1,13 @@
+import { IsBoolean, IsNotEmpty, IsString } from 'class-validator';
+
 export class TodoDTO {
+  @IsString({ message: 'should be string' })
+  @IsNotEmpty({ message: 'should be filled' })
   readonly title: string;
+
+  @IsBoolean({ message: 'should be boolean' })
   readonly isDone: boolean;
+
+  @IsString({ message: 'should be string' })
   readonly userId: string;
 }
--- server/src/user/user.dto.ts
+++ server/src/user/user.dto.ts
@@ -1,8 +1,18 @@
+import { IsNotEmpty, IsString, Length } from 'class-validator';
 import { ObjectId } from 'mongoose';
 
 export class UserDTO {
+  @IsString({ message: 'should be string' })
+  @IsNotEmpty({ message: 'should be filled' })
   readonly name: string;
+
+  @IsString({ message: 'should be string' })
+  @IsNotEmpty({ message: 'should be filled' })
   readonly tag: string;
+
+  @IsString({ message: 'should be string' })
+  @Length(6, 20, { message: 'no less then 6 and no more then 20 symbols' })
+  @IsNotEmpty({ message: 'should be filled' })
   readonly password: string;
 }
 
